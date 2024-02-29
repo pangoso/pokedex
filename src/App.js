08e7f6904738a2a2e4364271deca32a1d2ff09a2
pgrochowska <pauline.grochowska@gmail.com>
2024-02-29 12:56:22 +0100

    Initial page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Input, FormControl, MenuItem, Select } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Complete Pokémon Pokédex</h1>
+
+      <FormControl fullWidth>
+        <div>
+          Name:
+          <Input
+            autoFocus={true}
+            maxRows={1}
+            multiline={false}
+            name={"name"}
+            type={"text"}
+            // value={any}
+          />
+        </div>
+
+        <div>
+          Type:
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            // value={age}
+            label="Age"
+            // onChange={handleChange}
+           >
+             <MenuItem value={10}>Ten</MenuItem>
+             <MenuItem value={20}>Twenty</MenuItem>
+             <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </div>
+      </FormControl>
     </div>
   );
 }
